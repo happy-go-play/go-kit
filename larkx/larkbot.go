49fa5781c3a66808cb4188298318c0b71b80a63f
taobig <studentht@hotmail.com>
2025-06-13 11:39:58 +0800

    Update comments

--- larkx/larkbot.go
+++ larkx/larkbot.go
@@ -12,6 +12,9 @@ type LarkBot struct {
 }
 
 func NewLarkBot(larkBotConf LarkBotConfig) *LarkBot {
+	// https://open.larksuite.com/document/client-docs/bot-v3/add-custom-bot
+	// - 自定义机器人的频率控制和普通应用不同，为 100 次/分钟，5 次/秒。(The frequency control of the customized robot is different from the normal application, which is 100 times/minute and 5 times/second.)
+	// - 请求体大小不能超过 20 K。
 	return &LarkBot{
 		webhook: larkBotConf.Webhook,
 		secret:  larkBotConf.Secret,
@@ -19,9 +22,6 @@ func NewLarkBot(larkBotConf LarkBotConfig) *LarkBot {
 }
 
 func (a LarkBot) SendTextMessage(text string) error {
-	// https://open.larksuite.com/document/client-docs/bot-v3/add-custom-bot
-	// The frequency control of the customized robot is different from the normal application, which is 100 times/minute and 5 times/second.
-	// 自定义机器人的频率控制和普通应用不同，为 100 次/分钟，5 次/秒。
 	bot := lark.NewNotificationBot(a.webhook)
 	msgBuffer := lark.NewMsgBuffer(lark.MsgText)
 	if a.secret != "" {
