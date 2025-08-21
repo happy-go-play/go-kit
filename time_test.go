477778f4a9403d43f5e2c9990696cb83ae133009
taobig <studentht@hotmail.com>
2025-08-21 16:35:00 +0800

    Add FormatCompactRFC3339 and FormatCompactRFC3339Nano functions for compact time formatting

--- time_test.go
+++ time_test.go
@@ -1,9 +1,10 @@
 package gokit
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestStartOfDayInLocation(t *testing.T) {
@@ -126,4 +127,80 @@ func TestFormatDuration(t *testing.T) {
 		require.Equal(t, expected, result, "Expected %s but got %s", expected, result)
 	}
 
+}
+
+func TestFormatCompactRFC3339(t *testing.T) {
+	{
+		cst := time.FixedZone("CST", 8*3600) // 中国标准时间 (CST) UTC+8
+		t1 := FormatCompactRFC3339(time.Date(2025, 8, 20, 11, 24, 42, 0, cst))
+		expected1 := "2025-08-20T11:24:42+08"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+
+	{
+		ist := time.FixedZone("IST", 5*3600+30*60) // 印度标准时间 (IST) UTC+5:30
+		t1 := FormatCompactRFC3339(time.Date(2025, 8, 20, 11, 24, 42, 0, ist))
+		expected1 := "2025-08-20T11:24:42+05:30"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+
+	{
+		// UTC 时间
+		t1 := FormatCompactRFC3339(time.Date(2025, 8, 20, 11, 24, 42, 0, time.UTC))
+		expected1 := "2025-08-20T11:24:42Z"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+
+	{
+		brt := time.FixedZone("BRT", -3*3600) // 巴西利亚时间 (BRT) UTC-3
+		t1 := FormatCompactRFC3339(time.Date(2025, 8, 20, 11, 24, 42, 0, brt))
+		expected1 := "2025-08-20T11:24:42-03"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+}
+
+func TestFormatCompactRFC3339Nano(t *testing.T) {
+	{
+		cst := time.FixedZone("CST", 8*3600) // 中国标准时间 (CST) UTC+8
+		t1 := FormatCompactRFC3339Nano(time.Date(2025, 8, 20, 11, 24, 42, 123456789, cst))
+		expected1 := "2025-08-20T11:24:42.123456789+08"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+
+	{
+		ist := time.FixedZone("IST", 5*3600+30*60) // 印度标准时间 (IST) UTC+5:30
+		t1 := FormatCompactRFC3339Nano(time.Date(2025, 8, 20, 11, 24, 42, 123456789, ist))
+		expected1 := "2025-08-20T11:24:42.123456789+05:30"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+
+	{
+		// UTC 时间
+		t1 := FormatCompactRFC3339Nano(time.Date(2025, 8, 20, 11, 24, 42, 123456789, time.UTC))
+		expected1 := "2025-08-20T11:24:42.123456789Z"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
+
+	{
+		brt := time.FixedZone("BRT", -3*3600) // 巴西利亚时间 (BRT) UTC-3
+		t1 := FormatCompactRFC3339Nano(time.Date(2025, 8, 20, 11, 24, 42, 123456789, brt))
+		expected1 := "2025-08-20T11:24:42.123456789-03"
+		if t1 != expected1 {
+			t.Errorf("Expected %s but got %s", expected1, t1)
+		}
+	}
 }
