70e7e6f654d5c3a227b252443dc34773ac73c834
taobig <studentht@hotmail.com>
2025-11-18 17:20:03 +0800

    Add thread-safe LockedMap implementation with generics support

---
+++ concurrent-map/lockedmap/lockedmap.go
@@ -0,0 +1,94 @@
+package lockedmap
+
+import (
+	"sync"
+
+	"golang.org/x/exp/maps"
+)
+
+// LockedMap Thread-safe map with generics support
+type LockedMap[K comparable, V any] struct {
+	data map[K]V
+	mu   sync.RWMutex
+}
+
+func New[K comparable, V any]() *LockedMap[K, V] {
+	return &LockedMap[K, V]{
+		data: make(map[K]V),
+		mu:   sync.RWMutex{},
+	}
+}
+
+func (m *LockedMap[K, V]) Exists(key K) bool {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	_, ok := m.data[key]
+	return ok
+}
+
+func (m *LockedMap[K, V]) Get(key K) (V, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	value, ok := m.data[key]
+	if !ok {
+		return value, false
+	}
+
+	return value, true
+}
+
+func (m *LockedMap[K, V]) Set(key K, value V) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.data[key] = value
+}
+
+func (m *LockedMap[K, V]) Remove(key K) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	delete(m.data, key)
+}
+
+func (m *LockedMap[K, V]) RemoveAll() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	//clear(m.data) // since go 1.21
+	m.data = make(map[K]V)
+}
+
+func (m *LockedMap[K, V]) Size() int {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return len(m.data)
+}
+
+func (m *LockedMap[K, V]) Keys() []K {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return maps.Keys(m.data)
+}
+
+func (m *LockedMap[K, V]) Values() []V {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return maps.Values(m.data)
+}
+
+func (m *LockedMap[K, V]) Range(f func(key K, value V) bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	for key, value := range m.data {
+		if !f(key, value) {
+			break
+		}
+	}
+}
---
+++ concurrent-map/lockedmap/lockedmap_test.go
@@ -0,0 +1,84 @@
+package lockedmap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLockedMap_Exists(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	if !m.Exists("a") {
+		t.Errorf("Exists failed")
+	}
+}
+
+func TestLockedMap_Set(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	if v, ok := m.Get("a"); !ok || v != 1 {
+		t.Errorf("Get failed")
+	}
+}
+
+func TestLockedMap_Get(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	if v, ok := m.Get("a"); !ok || v != 1 {
+		t.Errorf("Get failed")
+	}
+}
+
+func TestLockedMap_Remove(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	m.Remove("a")
+	if m.Exists("a") {
+		t.Errorf("Remove failed")
+	}
+}
+
+func TestLockedMap_RemoveAll(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	m.RemoveAll()
+	if m.Exists("a") {
+		t.Errorf("RemoveAll failed")
+	}
+	assert.Equal(t, 0, m.Size())
+}
+
+func TestLockedMap_Size(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	if m.Size() != 1 {
+		t.Errorf("Size failed")
+	}
+}
+
+func TestLockedMap_Keys(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	if len(m.Keys()) != 1 {
+		t.Errorf("Keys failed")
+	}
+}
+
+func TestLockedMap_Values(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	if len(m.Values()) != 1 {
+		t.Errorf("Values failed")
+	}
+}
+
+func TestLockedMap_Range(t *testing.T) {
+	m := New[string, int]()
+	m.Set("a", 1)
+	m.Set("b", 2)
+	m.Set("c", 3)
+	m.Range(func(key string, value int) bool {
+		return true
+	})
+}
